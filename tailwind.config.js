6aa3beb8a0e7480f9dce4bebc4a26d1147d51904
Psalmantha <psalmanthaallaine03@gmail.com>
2024-11-17 18:10:45 +0800

    feat: extend theme with custom font family and color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Onest: ["Onest", "serif"],
+      },
+      colors: {
+        "dowear-red": "#DE0F3F"
+      },
+    },
   },
   plugins: [],
 }
