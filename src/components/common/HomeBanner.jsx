874976b7371c9de42758c8e117b4908218ab168b
Psalmantha <ipongpsalm1203@gmail.com>
2024-12-06 11:00:00 +0800

    feat: unfinished homepage banner design

---
+++ src/components/common/HomeBanner.jsx
@@ -0,0 +1,9 @@
+function HomeBanner() {
+    return (
+        <div className="w-screen bg-dowear-red h-[500px] flex flex-row items-center justify-left px-20">
+            <h1 className="text-white text-6xl font-medium">Explore, Shop and Sell, and Repeat.</h1>
+        </div>
+    )
+}
+
+export default HomeBanner
