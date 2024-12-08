61ee05e5a1bb109ee5f196a7e6e037e84acb073e
Psalmantha <ipongpsalm1203@gmail.com>
2024-12-08 21:40:12 +0800

    feat: PrivateRoute to restrict ProfilePage access

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
-import SignUpPage from "./pages/SignupPage"
+import SignUpPage from './pages/SignupPage'
+import ViewProduct from './pages/ViewProduct'
+import PrivateRoute from './components/common/PrivateRoute'
+import ProfilePage from './pages/ProfilePage'
 
 function App() {
   return (
@@ -11,10 +14,15 @@ function App() {
         <Route path="/" element={<HomePage/>}/>
         <Route path="/login" element={<LoginPage/>}/>
         <Route path="/signup" element={<SignUpPage/>}/>
+        <Route path="/view" element={<ViewProduct/>}/>
+        <Route path="/profile" 
+          element={<PrivateRoute><ProfilePage/></PrivateRoute>}
+        />
+
       </Routes>
     </BrowserRouter>
-
     </>
   )
 }
+
 export default App
