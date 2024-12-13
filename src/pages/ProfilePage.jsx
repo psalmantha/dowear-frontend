af07ca4b850987038adac1494eee3915f7211764
Psalmantha <ipongpsalm1203@gmail.com>
2024-12-13 19:00:46 +0800

    feat: profile page sections for user's listings and ratings

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,34 +1,88 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import NavBar from '../components/common/NavBar'
 import Footer from '../components/common/Footer'
 import { useAuth } from '../context/AuthContext'
 import { FaLocationDot } from "react-icons/fa6"
+import { HiMiniStar } from "react-icons/hi2"
 
 function ProfilePage(){
     const { user } = useAuth();
+    const navigate = useNavigate();
+    const [activeSection, setActiveSection] = useState('listings');
 
-    if (!user) {
-        return <p>Loading...</p>;
-    } 
+    useEffect(() => {
+      if (!user) {
+          navigate('/login');
+      }
+  }, [user]);
 
     return(
         <>
          <div className="flex flex-col min-h-screen w-screen">
             <NavBar/>
-            <div className="relative mb-52"> {/* will fix mb */}
+            <div className="relative mb-32"> {/* will fix mb */}
                 <div className="bg-[#F7F7F7] h-60"/>
                 <div className="absolute flex items-center top-full left-32 transform -translate-y-1/3 z-10">
                         <div className="bg-[#D9D9D9] rounded-full h-48 w-48" />
                         <div className="ml-8 flex flex-col mt-10">
                             <h1 className="text-3xl font-medium">{user.username}</h1>
                             <div className="flex flex-row items-center mt-2">
+                              <div className="flex flex-row space-x-2 mr-5">
                                 <FaLocationDot className="text-[#D3D3D3] text-lg"/>
-                                <h1 className="text-md font-medium">{user.address}</h1>
-
+                                <p className="text-md text-[#C1C1C1]">{user.user_address || 'Not provided'}</p>
+                              </div>
+                              <div className="flex flex-row space-x-2">
+                                <HiMiniStar className="text-[#FFDE59] text-lg"/>
+                                <p className="text-md text-[#C1C1C1]">{user.user_rating !== null ? user.user_rating : 'No rating yet'}</p>
+                              </div>
+                                
                             </div>
                         </div>
                  </div>                
+            </div>
 
+            <div className="mt-9">
+              <div className="relative flex justify-center space-x-80">
+                  {/* listings text */}
+                  <button
+                      className={`text-xl font-semibold ${
+                          activeSection === 'listings' ? 'text-black' : 'text-gray-500'
+                      }`}
+                      onClick={() => setActiveSection('listings')}
+                  >
+                      Listings
+                  </button>
+                  {/* ratings text */}
+                  <button
+                      className={`text-xl font-semibold ${
+                          activeSection === 'ratings' ? 'text-black' : 'text-gray-500'
+                      }`}
+                      onClick={() => setActiveSection('ratings')}
+                  >
+                      Ratings
+                  </button>
+              </div>
+              {/* active underline */}
+              <div className="relative mt-2">
+                  <div
+                      className={`absolute h-[2.5px] bg-red-500 transition-all duration-300`}
+                      style={{
+                          width: activeSection === 'listings' ? '95px' : '83px',
+                          left: activeSection === 'listings' ? 'calc(50% - 243px)' : 'calc(50% + 155px)',
+                      }}
+                  />
+              </div>
+              {/* horizontal line */}
+              <div className="mt-2 border-t-2 border-gray-300 w-screen" />
+              {/* conditionally render content */}
+              <div className="mt-6">
+                  {activeSection === 'listings' ? (
+                      <p className="text-center text-lg">No Listings yet</p>
+                  ) : (
+                      <p className="text-center text-lg">No Ratings yet</p>
+                  )}
+              </div>
             </div>
             <Footer/>
         </div>
