94559b6cfc5c2458a7bc7e4fc482982e8bf01259
Psalmantha <ipongpsalm1203@gmail.com>
2024-12-17 22:43:04 +0800

    feat: add routes for cart, checkout, and dynamic product pages with private routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import SignUpPage from './pages/SignupPage'
 import ViewProduct from './pages/ViewProduct'
 import PrivateRoute from './components/common/PrivateRoute'
 import ProfilePage from './pages/ProfilePage'
+import CartPage from './pages/CartPage'
+import CheckoutPage from './pages/CheckoutPage'
+import Page from './pages/Page'
 
 function App() {
   return (
@@ -14,11 +17,11 @@ function App() {
         <Route path="/" element={<HomePage/>}/>
         <Route path="/login" element={<LoginPage/>}/>
         <Route path="/signup" element={<SignUpPage/>}/>
-        <Route path="/view" element={<ViewProduct/>}/>
-        <Route path="/profile" 
-          element={<PrivateRoute><ProfilePage/></PrivateRoute>}
-        />
-
+        <Route path="/page" element={<Page/>}/>
+        <Route path="/product/:productId" element={<ViewProduct/>}/>
+        <Route path="/profile" element={<PrivateRoute><ProfilePage/></PrivateRoute>}/>
+        <Route path="/cart" element={<PrivateRoute><CartPage/></PrivateRoute>}/>
+        <Route path="/checkout" element={<PrivateRoute><CheckoutPage/></PrivateRoute>}/>
       </Routes>
     </BrowserRouter>
     </>
